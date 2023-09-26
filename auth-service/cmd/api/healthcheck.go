832dcee100857f6699659bc5cb54483549d8c2d6
Réda Maizate <reda.maizate@affluences.com>
2023-09-26 19:22:11 +0200

    fix: added logs to auth-service containers

--- auth-service/cmd/api/healthcheck.go
+++ auth-service/cmd/api/healthcheck.go
@@ -8,6 +8,7 @@ import (
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	app.logger.Println("healthcheckHandler called")
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
